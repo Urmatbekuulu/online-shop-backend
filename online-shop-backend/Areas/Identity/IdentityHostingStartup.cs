3ce01c579c3e2ba5596f1e6dd4611ed8cf4fe465
Urmatbekuulu <raatbekurmatbekuulu@gmail.com>
2022-04-07 12:59:18 +0600

    initital scaffolding of identity

---
+++ online-shop-backend/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using online_shop_backend.Data;
+
+[assembly: HostingStartup(typeof(online_shop_backend.Areas.Identity.IdentityHostingStartup))]
+namespace online_shop_backend.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
